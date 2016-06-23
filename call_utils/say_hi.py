ebabdd3cb9f82b00d80f8ab079ff3794d9078b17
matt rubin <matt@vital.enterprises>
2016-06-22 22:38:04 -0700

    added module architecture

---
+++ call_utils/say_hi.py
@@ -0,0 +1,4 @@
+# Say hi to the caller
+def say_hi(response):
+	response.say("Hello person.")
+	return response
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import twilio.twiml
+from call_utils import say_hi
 
 app = Flask(__name__)
 
@@ -7,13 +8,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def run_server():
 	response = twilio.twiml.Response()
-	# response.say("Hello fellow human.")
 	say_hi(response)
 	return str(response)
-
-def say_hi(response):
-	response.say("Hello person.")
-	return response
 
 if __name__ == "__main__":
 	app.run(debug=True)
