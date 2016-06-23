c97275a2d09fe2dc675311fdf5aa9c1d87a751c2
matt rubin <matt@vital.enterprises>
2016-06-23 01:21:18 -0700

    monitor serving setup

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import twilio.twiml
 from call_utils import say_hi
 
 app = Flask(__name__)
 
-# This is the entry point for our app
-@app.route('/', methods=['GET', 'POST'])
-def run_server():
+@app.route('/voice', methods=['POST'])
+def voice_response():
 	response = twilio.twiml.Response()
 	say_hi(response)
 	return str(response)
+
+# This is the entry point for our app
+@app.route('/client', methods=['GET'])
+def web_client():
+	return render_template("call_monitor/call_monitor.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
